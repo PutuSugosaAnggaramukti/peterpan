e13e99e0fac96005fe21bb0fd5465b701eeef149
putuanggaramukti <putu.sugosa@si.ukdw.ac.id>
2021-06-11 23:56:30 +0700

    project peterpan

---
+++ app/Http/Controllers/homecontroller.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class homecontroller extends Controller
+{
+    public function view(){
+        return view('home_login');
+    }
+
+    public function index_login(){
+        return view('login.login');
+    }
+
+    public function loginuser(){
+        return view('login.loginuser');
+    }
+
+    public function dashboard(){
+        return view('dashboard.dashboard');
+    }
+}
---
+++ app/Http/Controllers/mhs_controller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class mhs_controller extends Controller
+{
+    public function index(){
+        
+    }
+}
